2e8b7ff84be900adc08ab1a8355b5cd379d54cd9
William Silversmith <william.silversmith@gmail.com>
2025-02-26 18:06:08 -0500

    feat: add GPL-compliant navis converter
    No linkage at the library level. Code created from documentation
    and reverse engineering.

--- osteoid/skeleton.py
+++ osteoid/skeleton.py
@@ -1052,6 +1052,30 @@ class Skeleton:
     return skeletons
 
   @classmethod
+  def from_navis(self, navis_skel):
+    """
+    Convert navis skeletons to osteoid.Skeleton. This
+    should be more efficient than the SWC interchange method.
+    """
+    vertex_types = None
+    if len(navis_skel.nodes.type):
+      # 'root', 'slab', 'branch', 'end', first letter of each
+      vertex_types = [ ord(t[0]) for t in navis_skel.nodes.type ] 
+      mapping = np.zeros(255, dtype=np.uint8)
+      mapping[ord('s')] = 7 # custom
+      mapping[ord('r')] = 8 # custom
+      mapping[ord('b')] = 5 # fork point
+      mapping[ord('t')] = 6 # end point
+      vertex_types = mapping[vertex_types]
+
+    return Skeleton(
+      vertices=navis_skel.vertices, 
+      edges=navis_skel.edges - 1,
+      radii=navis_skel.nodes.radius.to_numpy(),
+      vertex_types=vertex_types,
+    )
+
+  @classmethod
   def from_swc(self, swcstr):
     """
     The SWC format was first defined in 
@@ -1123,7 +1147,7 @@ class Skeleton:
 
   def to_swc(self, contributors=""):
     """
-    Prototype SWC file generator. 
+    SWC file generator. 
 
     The SWC format was first defined in 
     
